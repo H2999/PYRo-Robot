3e0848e435eea5a8ab10675944da5aa4402faf37
H29 <811416940@qq.com>
2026-04-07 12:51:45 +0800

    暂存

---
+++ Robot/UAV/gimbal/feedforward_kf/forecast_v.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 1 on 2026/4/6.
+//
+
+#include "forecast_v.h"
---
+++ Robot/UAV/gimbal/feedforward_kf/forecast_v.h
@@ -0,0 +1,8 @@
+//
+// Created by 1 on 2026/4/6.
+//
+
+#ifndef PYRO_ROBOT_FORECAST_V_H
+#define PYRO_ROBOT_FORECAST_V_H
+
+#endif //PYRO_ROBOT_FORECAST_V_H
